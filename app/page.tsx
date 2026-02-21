b98bd777789fe07ff55271b0d6bf26d587e4cb10
Taylor <taylormo@unc.edu>
2026-02-21 12:54:33 -0500

    created all the pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu"
 
 export default function Home() {
   return (
-    <div>
-      <h1>This page works</h1>
-    </div>
+    <>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      
+      <div>
+        <h1>This page works</h1>
+      </div>
+    </>
   );
 }
---
+++ app/resumereview.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+    return (
+        <div>
+            <h1>This page works</h1>
+        </div>
+    );
+}
